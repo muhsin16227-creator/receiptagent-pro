ef1a09594849aace964807554d041120d8c59207
muhsin16227-creator <muhsin16227@gmail.com>
2026-06-04 01:48:33 +0300

    Update database.js

--- database.js
+++ database.js
@@ -1,7 +1,7 @@
 const Database = require('better-sqlite3');
 const path = require('path');
 
-const db = new Database(process.env.DATABASE_PATH || path.join(__dirname, 'receiptagent.db'));
+const db = new Database(path.join(__dirname, 'receiptagent.db'));
 
 // Create tables
 db.exec(`
